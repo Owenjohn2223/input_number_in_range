8219f3ce5dd4f7e644a60d710ea054e310e0f8bb
Owenjohn2223 <owenjohngalamgam@gmail.com>
2024-10-31 17:29:33 +0800

    Changed variable, used list to be manageable

--- input_range_number.py
+++ input_range_number.py
@@ -1,24 +1,20 @@
 
-range_1_10 = 0
-range_11_20 = 0
-range_21_30 = 0
-range_31_40 = 0
-range_41_50 = 0
+range_numbers = [0] * 5
 
 while True:
     try:
         input_number = int(input("Input a number between 1 and 50 (or a number outside of range to exit): "))
 
         if 1 <= input_number <= 10:
-            range_1_10 += 1
+            range_numbers[0] += 1
         elif 11 <= input_number <= 20:
-            range_11_20 += 1
+            range_numbers[1] += 1
         elif 21 <= input_number <= 30:
-            range_21_30 += 1
+            range_numbers[2] += 1
         elif 31 <= input_number <= 40:
-            range_31_40 += 1
+            range_numbers[3] += 1
         elif 41 <= input_number <= 50:
-            range_41_50 += 1
+            range_numbers[4] += 1
         else:
             print("///Exiting Program...///")
             break 
@@ -27,9 +23,9 @@ while True:
         print("///Input Invalid///")
 
 print("--- Summary of inputted numbers in the following range ---")
-print(f"\t1 - 10 = {range_1_10} ")
-print(f"\t11 - 20 = {range_11_20} ")
-print(f"\t21 - 30 = {range_21_30} ")
-print(f"\t31 - 40 = {range_31_40} ")
-print(f"\t41 - 50 = {range_41_50} ")
+print(f"\t1 - 10 = {range_numbers[0]} ")
+print(f"\t11 - 20 = {range_numbers[1]} ")
+print(f"\t21 - 30 = {range_numbers[2]} ")
+print(f"\t31 - 40 = {range_numbers[3]} ")
+print(f"\t41 - 50 = {range_numbers[4]} ")
 
