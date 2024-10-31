31a2e27608c22a361eff196062963fd53b26ea2d
Owenjohn2223 <owenjohngalamgam@gmail.com>
2024-10-31 16:47:04 +0800

    Range from 1-50 - all working!

--- input_range_number.py
+++ input_range_number.py
@@ -1,16 +1,24 @@
 
 range_1_10 = 0
 range_11_20 = 0
-
+range_21_30 = 0
+range_31_40 = 0
+range_41_50 = 0
 
 while True:
     try:
-        input_number = int(input("Input a number between 1 and 20 (Input a number outside of range to exit): "))
+        input_number = int(input("Input a number between 1 and 50 (Input a number outside of range to exit): "))
 
         if 1 <= input_number <= 10:
             range_1_10 += 1
         elif 11 <= input_number <= 20:
             range_11_20 += 1
+        elif 21 <= input_number <= 30:
+            range_21_30 += 1
+        elif 31 <= input_number <= 40:
+            range_31_40 += 1
+        elif 41 <= input_number <= 50:
+            range_41_50 += 1
         else:
             break 
 
@@ -18,5 +26,8 @@ while True:
         print("Input Invalid")
 
 print("Summary of inputted numbers in the following range:")
-print(f"- 1-10 = {range_1_10} ")
-print(f"- 11-20 = {range_11_20} ")
+print(f"1 - 10 = {range_1_10} ")
+print(f"11 - 20 = {range_11_20} ")
+print(f"21 - 30 = {range_21_30} ")
+print(f"31 - 40 = {range_31_40} ")
+print(f"41 - 50 = {range_41_50} ")
