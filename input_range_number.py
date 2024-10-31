8c2d26765f201700d3b65e88ab03be307d0a4395
Owenjohn2223 <owenjohngalamgam@gmail.com>
2024-10-31 16:15:57 +0800

    Plotted the algorithm for the whole program.

---
+++ input_range_number.py
@@ -0,0 +1,5 @@
+# The program requests users to type an input that ranges from 1-50. If the input is invalid (not an integer) it will display print message, warning the user to only input an integer .
+
+# When the user decides to end the program, which in this case, the user will input a number that ranges outside the set range to end the loop.
+
+# After the user decides to end the loop, the program will now display how many inputted numbers are in the following range.
