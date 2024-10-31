2b9023c96c63c621f5a8d00377a3b9e5f1dda88c
Owenjohn2223 <owenjohngalamgam@gmail.com>
2024-10-31 16:39:00 +0800

    tried to range it from 1 to 20 first - its working!

--- input_range_number.py
+++ input_range_number.py
@@ -1,5 +1,22 @@
-# The program requests users to type an input that ranges from 1-50. If the input is invalid (not an integer) it will display print message, warning the user to only input an integer .
 
-# When the user decides to end the program, which in this case, the user will input a number that ranges outside the set range to end the loop.
+range_1_10 = 0
+range_11_20 = 0
 
-# After the user decides to end the loop, the program will now display how many inputted numbers are in the following range.
+
+while True:
+    try:
+        input_number = int(input("Input a number between 1 and 20 (Input a number outside of range to exit): "))
+
+        if 1 <= input_number <= 10:
+            range_1_10 += 1
+        elif 11 <= input_number <= 20:
+            range_11_20 += 1
+        else:
+            break 
+
+    except ValueError:
+        print("Input Invalid")
+
+print("Summary of inputted numbers in the following range:")
+print(f"- 1-10 = {range_1_10} ")
+print(f"- 11-20 = {range_11_20} ")
