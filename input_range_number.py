6e95860d8f5a4bfdab3a90c20ae16da9e61945a8
Owenjohn2223 <owenjohngalamgam@gmail.com>
2024-10-31 17:00:59 +0800

    Added some changes to the prints for readability

--- input_range_number.py
+++ input_range_number.py
@@ -7,7 +7,7 @@ range_41_50 = 0
 
 while True:
     try:
-        input_number = int(input("Input a number between 1 and 50 (Input a number outside of range to exit): "))
+        input_number = int(input("Input a number between 1 and 50 (or a number outside of range to exit): "))
 
         if 1 <= input_number <= 10:
             range_1_10 += 1
@@ -20,14 +20,16 @@ while True:
         elif 41 <= input_number <= 50:
             range_41_50 += 1
         else:
+            print("///Exiting Program...///")
             break 
 
     except ValueError:
-        print("Input Invalid")
+        print("///Input Invalid///")
 
-print("Summary of inputted numbers in the following range:")
-print(f"1 - 10 = {range_1_10} ")
-print(f"11 - 20 = {range_11_20} ")
-print(f"21 - 30 = {range_21_30} ")
-print(f"31 - 40 = {range_31_40} ")
-print(f"41 - 50 = {range_41_50} ")
+print("--- Summary of inputted numbers in the following range ---")
+print(f"\t1 - 10 = {range_1_10} ")
+print(f"\t11 - 20 = {range_11_20} ")
+print(f"\t21 - 30 = {range_21_30} ")
+print(f"\t31 - 40 = {range_31_40} ")
+print(f"\t41 - 50 = {range_41_50} ")
+
